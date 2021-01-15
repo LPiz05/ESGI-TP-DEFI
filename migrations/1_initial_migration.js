900d63eabcf1f1cdb16798706b509f90806f68e2
Luis <pereiravazluis@gmail.com>
2021-01-15 14:39:30 +0100

    Prepare file to lunch migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
 const Migrations = artifacts.require("Migrations");
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(DaiToken);
+  deployer.deploy(DappToken);
 };
