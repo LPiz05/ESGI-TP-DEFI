bffd815088110d4a406964d5f902a03626979461
Luis <pereiravazluis@gmail.com>
2021-01-15 15:44:34 +0100

    Prepare migration contracts and transfert dai and dapp

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const DaiToken = artifacts.require("DaiToken");
-const DappToken = artifacts.require("DappToken");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(DaiToken);
-  deployer.deploy(DappToken);
 };
